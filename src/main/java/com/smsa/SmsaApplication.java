88fef98a47fdbf8179c45c4ca46388442430a820
Garikapati Gopi <gopi.g@eleserv.in>
2025-08-28 15:05:37 +0530

    changes for swagger

--- src/main/java/com/smsa/SmsaApplication.java
+++ src/main/java/com/smsa/SmsaApplication.java
@@ -4,13 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.smsa"})
+@EnableSwagger2
 public class SmsaApplication extends SpringBootServletInitializer{
 
 
     public static void main(String[] args) {
         SpringApplication.run(SmsaApplication.class, args);
+    }
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.smsa"))
+                .paths(PathSelectors.any()).build().pathMapping("/");
     }
 
 //    @Override
